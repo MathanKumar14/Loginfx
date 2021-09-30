2369e1b53d5ee0847cb1f6ce10e94628af17b6cd
MathanKumar14 <mathankumar2k2@gmail.com>
2021-09-30 15:41:15 +0530

    Test commit

--- src/main/java/myapp/loginfx/Main.java
+++ src/main/java/myapp/loginfx/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
-        stage.setTitle("XOXO tickets");
+        stage.setTitle("My tickets");
         stage.setScene(scene);
         stage.show();
     }
